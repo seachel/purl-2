834011d806d7a0138f8a8f7a971d5e40b08e363c
Chelsea <chelsea.battell@gmail.com>
2017-03-23 17:50:16 -0400

    Errors being displayed in space on page.

--- logic.js
+++ logic.js
@@ -199,47 +199,6 @@ function Pattern(castOnValue) {
 		return rows[rows.length - 1]
 	}
 
-	function RemainingStitchesError(stitchesRemaining, rowIndex)
-	{
-		var errorMessage = "Error: " + stitchesRemaining + " stitches remaining in row " + (rowIndex + 1);
-
-		return RowError(errorMessage, rowIndex);
-	}
-
-	function AdjacentRowError(row1StitchesEnd, row1Index, row2StitchesStart)
-	{
-		var errorMessage = "Error: Adjacent row error. " + row1StitchesEnd
-							+ " stitches at the end of row " + (row1Index + 1)
-							+ "and " + row2StitchesStart + " stitches at the start of row "
-							+ (row1Index + 2);
-
-		return RowError(errorMessage, row1Index);
-	}
-
-	function checkPatternCorrectness()
-	{
-		// TODO: will be run whenever a new row is added
-		//	- checks that final number of stitches in row matches the starting number of stitches in the next
-		//		-> true by construction?
-		// - when an error is added, want it to be displayed in the front end
-		for (let rowIndex = 0; rowIndex < rows.length; rowIndex++)
-		{
-			if (rows[rowIndex].stitchesRemaining > 0)
-			{
-				errors.push(RemainingStitchesError(rows[rowIndex].stitchesRemaining, rowIndex));
-			}
-			if (rowIndex + 1 < rows.length)
-			{
-				var currentRowStitchesEnd = rows[rowIndex].stitchesEnd;
-				var nextRowStitchesStart = rows[rowIndex + 1].stitchesStart;
-
-				if (currentRowStitchesEnd !== nextRowStitchesStart)
-				{
-					errors.push(AdjacentRowError(currentRowStitchesEnd, rowIndex, nextRowStitchesStart))
-				}
-			}
-		}
-	}
 
 	var PublicAPI = {
 		rows: rows,
@@ -265,18 +224,85 @@ function Pattern(castOnValue) {
 }
 
 
-function Error(message)
+// ------------------------------------------------------
+// Correctness
+// ------------------------------------------------------
+function checkPatternCorrectness(pattern)
 {
-	return {
-		message: message,
+	function RowError(message, rowIndex = currentRowIndex)
+	{
+		var errorMessage = "Error in row " + rowIndex + ": " + message;
+
+		return {
+			message: errorMessage,
+			rowIndex: rowIndex
+		};
 	}
-}
 
-function RowError(message, rowIndex = currentRowIndex)
-{
-	var errorMessage = "Error in row " + rowIndex + ": " + message;
+	function OverusedStitchesError(stitchesRemaining, rowIndex)
+	{
+		var errorMessage = "Overuse of stitches - " + stitchesRemaining + " stitches remaining in row " + (rowIndex + 1);
 
-	return Error(errorMessage);
+		return RowError(errorMessage, rowIndex);
+	}
+
+	function RemainingStitchesError(stitchesRemaining, rowIndex)
+	{
+		var errorMessage = "Unused stitches - " + stitchesRemaining + " stitches remaining in row " + (rowIndex + 1);
+
+		return RowError(errorMessage, rowIndex);
+	}
+
+	function AdjacentRowError(row1StitchesEnd, row1Index, row2StitchesStart)
+	{
+		var errorMessage = "Adjacent row stitch count mismatch - " + row1StitchesEnd
+							+ " stitches at the end of row " + (row1Index + 1)
+							+ "and " + row2StitchesStart + " stitches at the start of row "
+							+ (row1Index + 2);
+
+		return RowError(errorMessage, row1Index);
+	}
+
+	// TODO: will be run whenever a new row is added
+	//	- checks that final number of stitches in row matches the starting number of stitches in the next
+	//		-> true by construction?
+	// - when an error is added, want it to be displayed in the front end
+	if (pattern)
+	{
+		for (let rowIndex = 0; rowIndex < pattern.rows.length; rowIndex++)
+		{
+			if (pattern.rows[rowIndex].stitchesRemaining < 0)
+			{
+				pattern.errors.push(OverusedStitchesError(pattern.rows[rowIndex].stitchesRemaining, rowIndex));
+			}
+
+			if (rowIndex + 1 < pattern.rows.length)
+			{
+				var currentRowStitchesEnd = pattern.rows[rowIndex].stitchesEnd;
+				var nextRowStitchesStart = pattern.rows[rowIndex + 1].stitchesStart;
+
+				if (currentRowStitchesEnd !== nextRowStitchesStart)
+				{
+					pattern.errors.push(AdjacentRowError(currentRowStitchesEnd, rowIndex, nextRowStitchesStart))
+				}
+			}
+
+			if (rowIndex < pattern.rows.length && rowIndex !== currentRowIndex)
+			{
+				var currentRowStitchesRemaining = pattern.rows[rowIndex].stitchesRemaining;
+
+				if (currentRowStitchesRemaining > 0)
+				{
+					pattern.errors.push(RemainingStitchesError(currentRowStitchesRemaining, rowIndex));
+				}
+			}
+		}
+	}
+	else
+	{
+		// TODO: program error... checking correctness of undefined pattern
+		console.log("Pattern passed to correctness check is " + pattern);
+	}
 }
 
 
@@ -509,19 +535,7 @@ function AddError(errorMessage)
 	AddErrorToDisplay(newError);
 }
 
-function AddRowError(errorMessage)
-{
-	var newError = RowError(errorMessage, currentRowIndex + 1);
-
-	AddErrorToModel(newError);
-	AddErrorToDisplay(newError);
-}
-
-function AddErrorToModel(error)
-{
-	currentPattern.AddError(error);
-}
-
+// Necessary?
 function AddErrorToDisplay(error)
 {
 	var newNode = htmlNodeForError(error);
@@ -538,6 +552,7 @@ function htmlNodeForError(error)
 	return newNode;
 }
 
+// necessary?
 function ClearErrorDisplay()
 {
 	var errorDisplayNode = document.querySelector("#pattern-errors");
@@ -557,40 +572,55 @@ function ClearErrorDisplay()
 
 function UI_AddStitch(stitch)
 {
-	if (currentRowIndex >= 0)
+	if (currentPattern)
 	{
-		if (stitch.stitchesDropped <= getCurrentRow().stitchesRemaining)
+		if (currentRowIndex >= 0)
 		{
 			AddStitchToModel(stitch);
 			AddStitchToDisplay(stitch);
+	
+			checkPatternCorrectness(currentPattern);
+			var errorElement = document.querySelector("#pattern-errors");
+			errorElement.innerHTML = currentPattern.errors.map(e => e.message);
 		}
 		else
 		{
-			var errorMessage = "Can't add stitch " + stitch.stitchCode + " to row; need " + stitch.stitchesDropped + " stitches but only " + getCurrentRow().stitchesRemaining + " stitches remaining.";
-
-			AddRowError(errorMessage);
+			// Should have a different kind of error... UI error? should currentRowIndex always be nonneg?
+			console.log("No rows in pattern; can't add stitch " + stitch + " to current row.");
 		}
 	}
 	else
 	{
-		AddError("No rows in pattern; can't add stitch " + stitch + " to current row.");
+		// TODO: user error... no current pattern
+		console.log("No current pattern.");
 	}
 }
 
 function UI_AddNewRow()
 {
-	var newRow = Row(currentPattern.lastRowWidth);
+	if (currentPattern)
+	{
+		var newRow = Row(currentPattern.lastRowWidth);
+	
+		currentRowIndex++;
+	
+		AddRowToModel(newRow);
+		AddRowToDisplay(newRow);
 
-	currentRowIndex++;
-
-	AddRowToModel(newRow);
-	AddRowToDisplay(newRow);
+		checkPatternCorrectness(currentPattern);
+		var errorElement = document.querySelector("#pattern-errors");
+		errorElement.innerHTML = currentPattern.errors.map(e => e.message);
+	}
+	else
+	{
+		// TODO: user error... no current pattern
+		console.log("No current pattern.");
+	}
 }
 
 function UI_NewPattern()
 {
 	ClearPatternDisplay();
-	ClearErrorDisplay();
 
 	var newPattern = Pattern(GetCastOnValue());
 
